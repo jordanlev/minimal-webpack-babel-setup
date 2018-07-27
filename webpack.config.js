8fff481b6d1755c23cabffa782352620308209ee
Jordan Lev <github@jordanlev.com>
2018-07-26 23:48:56 -0500

    minor formatting tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+        loader: 'babel-loader'
       }
     ]
   },
