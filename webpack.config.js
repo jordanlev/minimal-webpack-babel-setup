661996100d34fa65e5f4c219c2ece37490435352
Jordan Lev <github@jordanlev.com>
2018-07-26 23:43:46 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
