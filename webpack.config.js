644427835608934ddf12f93797b86224dcf9831f
Jordan Lev <github@jordanlev.com>
2018-07-27 22:37:27 -0500

    move index.html outside of dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 module.exports = {
   entry: ['babel-polyfill', './src/index.js'],
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
-    filename: 'main.js'
+    filename: 'main.js',
   },
+  plugins: [
+    new CopyWebpackPlugin(['index.html']),
+  ],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
+        loader: 'babel-loader',
+      },
+    ],
   },
-  devServer: {
-    contentBase: './dist'
-  }
 };
